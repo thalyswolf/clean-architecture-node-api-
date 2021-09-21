3f58cfea69a66f15995fb15621345ee20c9f48a0
Thalys Wolf <thalys-externo@atarb2b.com>
2021-09-20 22:45:04 -0300

    Ensure login auth return 500 if email validator throws occur

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -38,6 +38,18 @@ const makeEmailValidator = () => {
   return emailValidatorSpy
 }
 
+const makeEmailValidatorWithError = () => {
+  class EmailValidatorSpy {
+    isValid (email) {
+      throw new Error()
+    }
+  }
+  const emailValidatorSpy = new EmailValidatorSpy()
+  emailValidatorSpy.isEmailValid = true
+
+  return emailValidatorSpy
+}
+
 const makeAuthUseCaseWithError = () => {
   class AuthUseCase {
     async auth (email, password) {
@@ -210,6 +222,23 @@ describe('Login router', () => {
 
     const sut = new LoginRouter(authUseCase, {})
 
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('Should return 500 if email validator throws', async () => {
+    const { authUseCase } = makeSut()
+    const emailValidator = makeEmailValidatorWithError()
+
+    const sut = new LoginRouter(authUseCase, emailValidator)
     const httpRequest = {
       body: {
         email: 'any_email@email.com',
